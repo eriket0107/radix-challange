905b86f060520e0feeef4843b3d00f0cff3828ca
Erik <ol.erik0107@gmail.com>
2024-09-27 08:20:39 -0300

    test: crete tests to fix sensor data

---
+++ src/use-cases/errors/csv-read-file-error.ts
@@ -0,0 +1,5 @@
+export class CSVReadFileError extends Error {
+  constructor() {
+    super('Error when reading CSV.')
+  }
+}
--- src/use-cases/fix-sensor-data-use-case/index.test.ts
+++ src/use-cases/fix-sensor-data-use-case/index.test.ts
@@ -3,6 +3,8 @@ import { beforeEach, describe, expect, it } from 'vitest'
 import { sensorDataEmptyMock } from '@/mocks/sensorDataMock'
 import { InMemorySensorRepository } from '@/repositories/in-memory/in-memory-sensor-repository'
 
+import { CSVReadFileError } from '../errors/csv-read-file-error'
+import { EmptyFilePathError } from '../errors/empty-file-path-error'
 import { FixSensorDataUseCase } from '.'
 
 let sensorRepository: InMemorySensorRepository
@@ -16,7 +18,6 @@ describe('Shoulbd be able to fix Sensor data Use Case', async () => {
 
   it('should be able to fix a Sensor Data values', async () => {
     const dataToFix = { ...sensorDataEmptyMock() }
-    console.log('dataToFix', dataToFix)
     sensorRepository.register(dataToFix)
     const { fixedSensorData } = await sut.execute({
       filePath: 'src/use-cases/fix-sensor-data-use-case/test.csv',
@@ -26,7 +27,33 @@ describe('Shoulbd be able to fix Sensor data Use Case', async () => {
     expect(fixedSensorData.equipmentId).toEqual(expect.any(String))
     expect(fixedSensorData.value).toEqual(expect.any(String))
     expect(fixedSensorData.timestamp).not.toHaveLength(0)
+  })
+  beforeEach(async () => {
+    sensorRepository = new InMemorySensorRepository()
+    sut = new FixSensorDataUseCase(sensorRepository)
+  })
 
-    console.log('fixedSensorData', fixedSensorData)
+  it('should not be able to fix a Sensor Data values for empty csv file fields', async () => {
+    const dataToFix = { ...sensorDataEmptyMock() }
+    sensorRepository.register(dataToFix)
+
+    expect(
+      async () =>
+        await sut.execute({
+          filePath: 'src/use-cases/fix-sensor-data-use-case/test-empty.csv',
+        }),
+    ).rejects.toBeInstanceOf(CSVReadFileError)
+  })
+
+  it('should not be able to fix a Sensor Data values for empty file path', async () => {
+    const dataToFix = { ...sensorDataEmptyMock() }
+    sensorRepository.register(dataToFix)
+
+    expect(
+      async () =>
+        await sut.execute({
+          filePath: '',
+        }),
+    ).rejects.toBeInstanceOf(EmptyFilePathError)
   })
 })
